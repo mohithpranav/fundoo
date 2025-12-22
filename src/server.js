959eea5870954a143ec53387cd1787c7044aa8cf
Mohith Pranav <pranavmohith@gmail.com>
2025-12-22 09:17:56 +0530

    updated

--- src/server.js
+++ src/server.js
@@ -4,9 +4,7 @@ import express from "express";
 import router from "./routes/userRoutes.js";
 
 const app = express();
-mongoose.connect(
-  "mongodb+srv://admin:JgGeEEzS1xeBhfEb@cluster0.zpsb0jr.mongodb.net/fundooDB"
-);
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(express.json());
 app.use("/", router);
